6553a7ac4c87754a4e9d85611584faadec8707a3
HughTB <hugh.baldwin77@gmail.com>
2024-01-03 17:22:56 +0000

    fix media scripts; add now playing to date

--- .scripts/media-previous.sh
+++ .scripts/media-previous.sh
@@ -4,6 +4,8 @@ NOTIF_FILE=/home/hugh/.scripts/notif-media
 
 playerctl previous
 
+sleep 1
+
 if test -f $NOTIF_FILE && [[ "$(cat $NOTIF_FILE)" =~ ^[0-9]+$ ]]; then
   notify-send -r $(cat $NOTIF_FILE) -p -e --app-name "$(playerctl status)" "$(playerctl metadata --format '{{artist}}')" "$(playerctl metadata --format '{{title}}')" -i "$(playerctl metadata --format '{{mpris:artUrl}}' | sed 's@file://@@g')" > $NOTIF_FILE
 else
